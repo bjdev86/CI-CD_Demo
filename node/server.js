c5c7052add3ef7031fc2d35876a16a5b88b78e75
bjdev86 <36616379+bjdev86@users.noreply.github.com>
2020-07-26 23:20:46 -0700

    Changed greeting

--- node/server.js
+++ node/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to the Park!\n');
+    res.end('Welcome to the Project!\n');
 });
 
 server.listen(port, hostname, () => {
