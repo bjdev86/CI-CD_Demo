c0e5bf23f1ef979d1d99e79c0974698e1ae6e3b1
bjdev86 <36616379+bjdev86@users.noreply.github.com>
2020-07-26 23:46:03 -0700

    Update server greeting

--- node/server.js
+++ node/server.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Welcome to the Project!\n');
+    res.end('Go Build a server!\n');
 });
 
 server.listen(port, hostname, () => {
